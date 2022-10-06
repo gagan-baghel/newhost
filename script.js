75606eb6726d1ab18b8f9b7f02616e11e0d5232f
Gagan Baghel <gagansinghbaghel15@gmail.com>
2022-10-06 15:33:03 +0530

    sc

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    doc = document.getElementById("leftmenu")
+    btn = document.getElementById("menubtn")
+    btn.classList.toggle("change");
+    doc.classList.toggle("lefthide")
+    doc.classList.toggle("leftshow")
+  }
